3c248e84bb6fc17f6dd5a30732e05b8855f54596
dana-alsibai <alsibaid@sheridancollege.ca>
2022-12-26 01:09:52 -0500

    connect to mongoDB and create route API

---
+++ crud_app/server/controller/controller.js
@@ -0,0 +1,24 @@
+var Userdb = require('../model/model');
+
+//create an API 
+//create and save user 
+
+exports.create = (req, res) => {
+
+}
+
+//retrieve and return all users / retrieve and return a single user
+exports.find = (req, res) => {
+
+}
+
+//update a new user by user id 
+exports.update = (req, res) => {
+
+}
+
+//delete a user with an id 
+
+exports.delete = (req, res) => {
+    
+}
