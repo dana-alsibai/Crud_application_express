1b167f188ea57e9a402c47b3bac9a3301c77ff04
dana-alsibai <alsibaid@sheridancollege.ca>
2022-12-26 01:24:56 -0500

    create user and save it to mongodb

--- crud_app/server/controller/controller.js
+++ crud_app/server/controller/controller.js
@@ -4,6 +4,32 @@ var Userdb = require('../model/model');
 //create and save user 
 
 exports.create = (req, res) => {
+    //validate the request
+    if(!req.body){
+        res.status(400).send({message: "Content can not be empty!"})
+        return;
+    }
+
+    //new user 
+    const user = new Userdb({
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.body.gender,
+        status: req.body.status
+    })
+
+    //save data to database 
+
+    user
+    .save(user)
+    .then(data => {
+        res.send(data)
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || "error while creating a user"
+        })
+    })
 
 }
 
@@ -20,5 +46,5 @@ exports.update = (req, res) => {
 //delete a user with an id 
 
 exports.delete = (req, res) => {
-    
+
 }
--- crud_app/server/route/router.js
+++ crud_app/server/route/router.js
@@ -34,4 +34,5 @@ route.get('/api/users', controller.find)
 route.put('/api/users/:id', controller.update)
 
 route.delete('/api/users/:id', controller.delete)
+
 module.exports = route
