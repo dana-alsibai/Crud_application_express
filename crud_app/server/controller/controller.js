e781603a1d1c4d8cf0e3f0c428d207316b88fb91
dana-alsibai <alsibaid@sheridancollege.ca>
2022-12-29 13:00:58 -0500

    finish update and get users

--- crud_app/server/controller/controller.js
+++ crud_app/server/controller/controller.js
@@ -35,12 +35,35 @@ exports.create = (req, res) => {
 
 //retrieve and return all users / retrieve and return a single user
 exports.find = (req, res) => {
-
+    Userdb.find()
+    .then(user => {
+        res.send(user)
+    })
+    .catch(err => {
+        res.status(500).send({message: err.message})
+    })
 }
 
 //update a new user by user id 
 exports.update = (req, res) => {
+    if(!req.body){
+        return res
+        .status(400)
+        .send({message: "Data cannot be empty"})
+    }
 
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id, req.body)
+    .then(data => {
+        if(!data){
+            return res.status(404).send({message: "user not found"})
+        }else {
+            res.send(data)
+        }
+    })
+    .catch(err => {
+        res.status(500).send({message: "Error update user information"})
+    })
 }
 
 //delete a user with an id 
